7bd6c22e7d29256de58133ec5eb8934e2427b98c
VictorMigwe <migwethumbi@gmail.com>
2023-05-03 15:50:57 +0300

    chapter 8 excercises

---
+++ PY4E/Chapter 8 - Lists/Exercise 8_6.py
@@ -0,0 +1,26 @@
+#Exercise 8_6: Rewrite the program that prompts the user for a list of numbers and prints out the maximum and minimum of the numbers at the end when the user enters “done”. Write the program to store the numbers the user enters in a list and use the max() and min() functions to compute the maximum and minimum numbers after the loop completes.
+
+#    Enter a number: 6
+#    Enter a number: 2
+#    Enter a number: 9
+#    Enter a number: 3
+#    Enter a number: 5
+#    Enter a number: done
+#    Maximum: 9.0
+#    Minimum: 2.0
+
+number_list = []
+while True :
+    try:
+        enter_number = input("Enter a Number: ")
+        if enter_number == "done" : break
+        if len(enter_number) < 1 : break
+        else:
+            enter_number = int(enter_number)
+    except:
+        print("invalid Input")
+        break
+    number_list.append(enter_number)
+    
+print("Maximum:", max(number_list))
+print("Minimum:", min(number_list))
